6466943cf43cc3c06743310826a0e8456cff22eb
unknown <acheole1@gmail.com>
2023-09-04 18:55:47 +0900

    add file mod

--- edittest.py
+++ edittest.py
@@ -14,3 +14,4 @@ print(flt_pi = 3.14)
 print(char_element = 'a')
 print(chars = "hello world")
 print("number : ", number_school, " name ", str_name, "bool : ", bool_view, " float : ", flt_pi, "charcter : " , char_element, "charcters : ", chars)
+111
