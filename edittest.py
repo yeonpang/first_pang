05f7ef2b6135c4f37a967f39ad82c49b63af53b2
unknown <acheole1@gmail.com>
2023-09-04 18:42:22 +0900

    add file mod

---
+++ edittest.py
@@ -0,0 +1,16 @@
+a = "hello"
+b = "world"
+print(a, b)
+
+print(a)
+print(b)
+print(b, a)
+print("python", a, b)
+
+printnumber_school = 2394584903
+str_name = "python"
+print(bool_view = False)
+print(flt_pi = 3.14)
+print(char_element = 'a')
+print(chars = "hello world")
+print("number : ", number_school, " name ", str_name, "bool : ", bool_view, " float : ", flt_pi, "charcter : " , char_element, "charcters : ", chars)
