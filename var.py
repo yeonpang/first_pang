ba17b939f05b812da46e1fe2db0d91a36329e39f
yeonpang <143787849+yeonpang@users.noreply.github.com>
2023-09-08 17:47:39 +0900

    Add files via upload

---
+++ var.py
@@ -0,0 +1,175 @@
+"""
+my_var = 10
+print(my_var)
+
+my_list = [1, 2, 3]
+print(my_list)
+print(*my_list)
+
+my_int = 10
+
+my_float = 3.14
+
+my_complex = 3 + 4j
+print(my_int)
+
+my_character = 'A'
+my_char = "@"
+print(my_char)
+
+my_string = 'Hello, world!'
+str_name = "python"
+
+my_bool = True
+bFlag = False
+print(bFlag)
+"""
+my_list = [10, 3, 8, 9, 42, "hello"]
+"""print(my_list[3])
+
+
+print(my_list. __len__())
+
+list_el = my_list[3]
+print(list_el)
+
+my_list = [10, 3, 8, 9, 42, "hello"]
+print(my_list)
+
+my_list[3] = 11
+
+print(my_list)
+
+my_list = [10, 3, 8, 9, 42, "hello"]
+
+print(my_list)
+
+n_add = my_list[3] + my_list[2]
+print(n_add)
+"""
+
+""" #print(my_list[3] - my_list[S])
+
+slice_ls = my_list[2:7]
+print(slice_ls)
+"""
+
+#my_list = [10, 3, 8, 9, 42, "hello"]
+#my_list = [10, 3, 8, 9, 42]
+#my_list = ['c', 'z', 'a']
+"""my_list = ['python', 'hello', 'apple']
+
+#my_list.insert(20, 'e')
+res = my_list.sort()
+#print(res)
+
+print(my_list)
+"""
+
+"""my_tup = (4, 2, 6, 1, "py", "w")
+
+print(my_tup.__len__())
+
+#my_tup[2] = 9
+
+
+print(my_tup.count(6))
+
+my_dict = {'key1': 'valuel', 'key2': 'value2'}
+
+my_item = my_dict.items()
+
+print(my_item)
+
+idx = ("key1", "key2", "key3")
+
+dicts = dict.fromkeys(idx, "0")
+
+print(dicts)
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+dicDate = {"name" : "python", "number" : 23564897}
+
+print(my_dict["key1"])
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+
+my_str = my_dict.get("key2")
+
+print(my_str)
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+
+print(my_dict.pop("key1"))
+
+print(my_dict)
+"""
+
+"""my_dict = {'key1': 'value1', 'key2': 'value2'}
+
+dicts = my_dict.copy()
+
+print(dicts)
+
+print(my_dict)
+"""
+
+"""my_dict = {'key1': 'value1', 'key2': 'value2'}
+dicDate = {"name" : "python", "number" : 23564897}
+
+my_dict["key3"] = "value3"
+
+print(my_dict)
+"""
+
+"""my_dict = {'key1': 'value1', 'key2': 'value2'}
+
+my_dict.setdefault("key3")
+
+print(my_dict)
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+
+my_dict.update({"key1" : "v4"})
+
+print(my_dict)
+"""
+
+"""my_dict = {'key1': 'value1', 'key2': 'value2'}
+
+del my_dict["key2"]
+
+print(my_dict)
+"""
+
+"""my_dict = {'key1': 'value1', 'key2': 'value2'}
+
+print("key2" in my_dict)
+
+print("key3" in my_dict)
+"""
+
+"""my_dict = {'key1': 'value1', 'key2': 'value2'}
+
+my_list = list(my_dict.keys())
+
+print(my_list)
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+
+my_list = list(my_dict.values())
+
+print(my_list)
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+
+my_set = set(my_dict.keys())
+
+print(my_set)
+"""
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+
+my_dict.clear()
+
+print(my_dict)
